c023f75017030445cf9d1d3914088f539ef2cbd6
dandeduck <danielmkhlv@gmail.com>
2021-01-04 23:40:59 +0200

    improved

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from videos import Video
 
 if __name__ == '__main__':
-    video = Video("https://www.youtube.com/watch?v=Y0muXSQRVEg&ab_channel=LinusTechTips", 'C:\\Users\\duck\\Documents\\test\\', 'C:\\Users\\duck\\Documents\\test\\', 1440)
+    video = Video("https://www.youtube.com/watch?v=Y0muXSQRVEg&ab_channel=LinusTechTips", 'C:\\Users\\duck\\Documents\\test\\', 'C:\\Users\\duck\\Documents\\test\\output\\', '1080p')
     video.downloadVideos()
     video.mergeClips()
--- videos.py
+++ videos.py
@@ -2,7 +2,6 @@ import pafy
 import moviepy.editor as mpe
 
 from files import VideoFile, AudioFile
-from util import resToPixels
 
 
 class Video:
@@ -10,40 +9,28 @@ class Video:
     videoFile = None
     audioFile = None
     outputFile = None
-    maxPixels = 0
+    resolution = ''
 
     def __init__(self, videoId, cacheDir, outputDir, resolution):
         self.video = pafy.new(videoId)
         self.videoFile = VideoFile(cacheDir, self.video.title)
         self.audioFile = AudioFile(cacheDir, self.video.title)
         self.outputFile = VideoFile(outputDir, self.video.title)
-        self.maxPixels = resToPixels(resolution)
+        self.resolution = resolution
 
     def downloadVideos(self):
-        print(self.__closestResolution())
-
-        self.__closestResolution().download(filepath=self.videoFile.path())
+        self.__closestResolutionStream().download(filepath=self.videoFile.path())
         self.video.getbestaudio().download(filepath=self.audioFile.path())
 
-    def __closestResolution(self):
-        smallestDelta = -1
-        closestStream = None
-
+    def __closestResolutionStream(self):
         for stream in self.video.videostreams:
-            delta = abs(self.__videoPixels(stream) - self.maxPixels)
-            if smallestDelta == -1 or delta < smallestDelta:
-                closestStream = stream
-                smallestDelta = delta
+            if stream.notes == self.resolution:
+                return stream
 
-        return closestStream
-
-    @classmethod
-    def __videoPixels(cls, stream):
-        dimensions = stream.resolution.split('x')
-        return int(dimensions[0]) * int(dimensions[1])
+        return self.video.getbestvideo()
 
     def mergeClips(self):
         videoClip = mpe.VideoFileClip(self.videoFile.path())
-        audioClip = mpe.VideoFileClip(self.audioFile.path())
+        audioClip = mpe.AudioFileClip(self.audioFile.path())
         mergedClip = videoClip.set_audio(audioClip)
-        mergedClip.write_videofile(self.outputFile.path())
+        mergedClip.write_videofile(self.outputFile.path(), fps=videoClip.fps)
