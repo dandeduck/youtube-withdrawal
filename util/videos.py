816a1b112f3e8157e77842a69d0ce96e84d06b9f
dandeduck <danielmkhlv@gmail.com>
2021-01-04 18:55:31 +0200

    improved video functionality

--- util/videos.py
+++ util/videos.py
@@ -9,16 +9,30 @@ class Video:
     videoFile = None
     audioFile = None
     outputFile = None
+    maxResWidth = 0
 
-    def __init__(self, videoId, cacheDir, outputDir):
+    def __init__(self, videoId, cacheDir, outputDir, maxResWidth):
         self.video = pafy.new(videoId)
         self.videoFile = VideoFile(cacheDir, self.video.title())
         self.audioFile = AudioFile(cacheDir, self.video.title())
         self.outputFile = VideoFile(outputDir, self.video.title())
+        self.maxResWidth = maxResWidth
 
     def downloadVideos(self):
-        self.video.getbestvideo().download(filepath=self.videoFile.path())
+        selectedStream = None
+        if self.videoWidth(self.video.getbestvideo()) <= self.maxResWidth:
+            selectedStream = self.video.getbestvideo()
+        else:
+            for stream in self.video.videostreams:
+                if(self.videoWidth(stream) >= self.maxResWidth):
+                    selectedStream = stream
+
+        selectedStream.download(filepath=self.videoFile.path())
         self.video.getbestaudio().download(filepath=self.audioFile.path())
+
+    @classmethod
+    def videoWidth(cls, stream):
+        return int(stream.resolution.split('x')[0])
 
     def mergeClips(self):
         videoClip = mpe.VideoFileClip(self.videoFile.path())
