fcdb1d9e8f5cd9aaaa9bdf7eb779afb42372b11c
dandeduck <danielmkhlv@gmail.com>
2021-01-05 02:37:47 +0200

    expended:

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from videos import Video
+from videos import Video, VideoSettings
 
 if __name__ == '__main__':
-    video = Video("https://www.youtube.com/watch?v=Y0muXSQRVEg&ab_channel=LinusTechTips", 'C:\\Users\\duck\\Documents\\test\\', 'C:\\Users\\duck\\Documents\\test\\output\\', '1080p')
+    video = Video("https://www.youtube.com/watch?v=0_RZby28s9E", VideoSettings('C:\\Users\\duck\\Documents\\test\\', 'C:\\Users\\duck\\Documents\\test\\output\\', '1080p60'))
     video.downloadVideos()
-    video.mergeClips()
+    video.outputVideo()
--- videos.py
+++ videos.py
@@ -4,6 +4,17 @@ import moviepy.editor as mpe
 from files import VideoFile, AudioFile
 
 
+class VideoSettings:
+    cacheDir = ''
+    outputDir = ''
+    resolution = ''
+
+    def __init__(self, cacheDir, outputDir, resolution):
+        self.cacheDir = cacheDir
+        self.outputDir = outputDir
+        self.resolution = resolution
+
+
 class Video:
     video = None
     videoFile = None
@@ -11,12 +22,12 @@ class Video:
     outputFile = None
     resolution = ''
 
-    def __init__(self, videoId, cacheDir, outputDir, resolution):
+    def __init__(self, videoId, settings):
         self.video = pafy.new(videoId)
-        self.videoFile = VideoFile(cacheDir, self.video.title)
-        self.audioFile = AudioFile(cacheDir, self.video.title)
-        self.outputFile = VideoFile(outputDir, self.video.title)
-        self.resolution = resolution
+        self.videoFile = VideoFile(settings.cacheDir, self.video.title)
+        self.audioFile = AudioFile(settings.cacheDir, self.video.title)
+        self.outputFile = VideoFile(settings.outputDir, self.video.title)
+        self.resolution = settings.resolution
 
     def downloadVideos(self):
         self.__closestResolutionStream().download(filepath=self.videoFile.path())
@@ -29,8 +40,32 @@ class Video:
 
         return self.video.getbestvideo()
 
-    def mergeClips(self):
+    def outputVideo(self):
         videoClip = mpe.VideoFileClip(self.videoFile.path())
         audioClip = mpe.AudioFileClip(self.audioFile.path())
         mergedClip = videoClip.set_audio(audioClip)
         mergedClip.write_videofile(self.outputFile.path(), fps=videoClip.fps)
+
+
+class VideoGenerator:
+    settings = None
+    videos = []
+
+    def __init__(self, settings, videoIds):
+        self.settings = settings
+        self.generateVideos(videoIds)
+
+    def generateVideos(self, videoIds):
+        for videoId in videoIds:
+            self.videos.append(Video(videoId, self.settings))
+
+    def downloadVideos(self):
+        for video in self.videos:
+            video.downloadVideos()
+
+    def outputVideos(self):
+        for video in self.videos:
+            try:
+                video.outputVideo()
+            except Exception as e:
+                continue #will be logged inthe future
